f2771d4f1b6820dc7b9f91178d44f5b663ced7bf
Salmansha08 <salmanwiharja8@gmail.com>
2024-09-13 15:28:22 +0700

    Finish with Error RabbitMQ

--- init.sql
+++ init.sql
@@ -10,20 +10,12 @@ CREATE TABLE product (
     price BIGINT
 );
 
-INSERT INTO product (name, qty, price) VALUES ('Sepatu', 10, 100000);
-INSERT INTO product (name, qty, price) VALUES ('Baju', 20, 150000);
-INSERT INTO product (name, qty, price) VALUES ('Celana', 15, 200000);
-
 \c user_service;
 CREATE TABLE user (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     alamat TEXT
 );
-
-INSERT INTO user (name, alamat) VALUES ('Frank', 'Jakarta');
-INSERT INTO user (name, alamat) VALUES ('Sarah', 'Bandung');
-INSERT INTO user (name, alamat) VALUES ('John', 'Surabaya');
 
 \c payment;
 CREATE TABLE payment (
@@ -35,8 +27,3 @@ CREATE TABLE payment (
     qty INT,
     bill BIGINT
 );
-
-INSERT INTO payment (paymentAt, userId, productId, price, qty, bill) 
-VALUES (NOW(), 1, 1, 100000, 2, 200000);
-INSERT INTO payment (paymentAt, userId, productId, price, qty, bill) 
-VALUES (NOW(), 2, 2, 150000, 1, 150000);
